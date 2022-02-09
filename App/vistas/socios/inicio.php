27fc40a6c6db2bcb997a39c5b0097d6a551c24d0
Gil Pablo <gilpablo2000.gpbp@gmail.com>
2022-02-09 12:35:08 +0100

    css

--- App/vistas/socios/inicio.php
+++ App/vistas/socios/inicio.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link rel="stylesheet" href="socio.css">
+    <link rel="stylesheet" href="<?php echo RUTA_URL ?>/css/estilos-socio.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Doppio+One&display=swap" rel="stylesheet">
-    <script src="funciones.js"></script>
+   
 
 
     <title>SOCIO</title>
