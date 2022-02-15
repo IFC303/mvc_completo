cf8cc48abf18c127b3f6717e744bc94e5fa1a0b3
sivlia <sielma712@gmail.com>
2022-02-15 10:17:23 +0100

    crud test

--- App/controladores/Entrenador.php
+++ App/controladores/Entrenador.php
@@ -25,17 +25,63 @@ class Entrenador extends Controlador
     }
 
 
-    public function test(){
-        
+    public function test(){  
         $test = $this->testModelo->obtenerTest();
-
         $this->datos['usuarios'] = $test;
-
         $this->vista('entrenadores/test', $this->datos);
     }
+
+
+    public function nuevo_test()
+    {
+        $this->datos['rolesPermitidos'] = [2];          // Definimos los roles que tendran acceso
+
+        if (!tienePrivilegios($this->datos['usuarioSesion']->id_rol, $this->datos['rolesPermitidos'])) {
+            redireccionar('/usuarios');
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $testNuevo = [
+                'id_test' => trim($_POST['id_test']),
+                'nombre' => trim($_POST['nombre'])
+            ];
+
+            if ($this->testModelo->agregarTest($testNuevo)) {
+                redireccionar('/entrenador');
+            } else {
+                die('Algo ha fallado!!!');
+            }
+        } else {
+            $this->datos['test'] = (object) [
+                'id_test' => '',
+                'nombre' => '',
+            ];
+            $this->datos['listaTest'] = $this->testModelo->obtenerTest();
+            $this->vista('entrenadores/nuevo_test', $this->datos);
+        }
+    }
+
+
+   public function borrar($id){
+       
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if ($this->testModelo->borrarTest($id)) {
+                redireccionar('/entrenador');
+            } else {
+                die('Algo ha fallado!!!');
+            }
+        } else {
+            $this->datos['usuarios'] = $this->testModelo->obtenerTestId($id);
+            $this->vista('entrenadores/test', $this->datos);
+        }
+    }
+
+
 
 
     public function mensajeria(){
         $this->vista('entrenadores/mensajeria', $this->datos);
     }
+
 }
