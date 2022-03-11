b933761fa7ad79815f2d45f6a7db3a09581cc9db
IFC303 <72009474+IFC303@users.noreply.github.com>
2022-03-11 09:29:03 +0100

    DB_HOST

--- App/config/configurar.php
+++ App/config/configurar.php
@@ -13,7 +13,7 @@ define('NOMBRE_SITIO', 'Tragamillas');
 
 // Configuracion de la Base de Datos
 //define('DB_HOST', '172.17.0.3');
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql');
 define('DB_USUARIO', 'root');
 define('DB_PASSWORD', 'toor');
 define('DB_NOMBRE', 'tragamillas2');
