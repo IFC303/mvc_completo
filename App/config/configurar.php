b64bcfc40f93eba54339d6dfb3179fe5309268f2
ivangy <ivansanchez989898@gmail.com>
2022-03-21 16:31:32 +0000

    configuracion

--- App/config/configurar.php
+++ App/config/configurar.php
@@ -5,16 +5,17 @@
 define('RUTA_APP', dirname(dirname(__FILE__)));
 
 // Ruta url, Ejemplo: http://localhost/daw2_mvc
-define('RUTA_URL', "https://{$_SERVER['HTTP_HOST']}/tragamillas");
+//define('RUTA_URL', "https://{$_SERVER['HTTP_HOST']}/tragamillas");
 //define('RUTA_URL', "http://localhost/tragamillas");
+define('RUTA_URL', 'https://192.168.100.153');
 //define('RUTA_URL', 'https://192.168.1.118');
 
 define('NOMBRE_SITIO', 'Tragamillas');
 
 
 // Configuracion de la Base de Datos
-//define('DB_HOST', '172.17.0.3');
-define('DB_HOST', 'mysql');
+define('DB_HOST', '172.17.0.3');
+//define('DB_HOST', 'mysql');
 define('DB_USUARIO', 'root');
 define('DB_PASSWORD', 'toor');
 //define('DB_PASSWORD', 'Admin1234');
