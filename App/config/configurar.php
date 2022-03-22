a28228505fd46ef0528736d69ee30040939cd16a
IFC303 <72009474+IFC303@users.noreply.github.com>
2022-03-22 09:50:56 +0100

    docker network

--- App/config/configurar.php
+++ App/config/configurar.php
@@ -1,26 +1,27 @@
 <?php
-//prueba casa
-//prueba casa rama
 // Ruta de la aplicacion
 define('RUTA_APP', dirname(dirname(__FILE__)));
 
 // Ruta url, Ejemplo: http://localhost/daw2_mvc
-//define('RUTA_URL', "https://{$_SERVER['HTTP_HOST']}/tragamillas");
-define('RUTA_URL', "http://localhost/tragamillas");
-//define('RUTA_URL', 'https://192.168.100.153');
-//define('RUTA_URL', 'https://192.168.1.118');
+define('RUTA_URL', "https://{$_SERVER['HTTP_HOST']}/tragamillas");
+
+// Ruta local
+// define('RUTA_URL', "http://localhost/tragamillas");
 
 define('NOMBRE_SITIO', 'Tragamillas');
 
+// Ruta host
+// define('DB_HOST', 'localhost');
 
-// Configuracion de la Base de Datos
-//define('DB_HOST', '172.17.0.3');
-//define('DB_HOST', 'mysql');
-define('DB_HOST', 'localhost');
+// Ruta bridge
+// define('DB_HOST', '172.17.0.3');
+
+// Ruta dinámica
+define('DB_HOST', 'mysql');
 define('DB_USUARIO', 'root');
 define('DB_PASSWORD', 'toor');
-//define('DB_PASSWORD', 'Admin1234');
 define('DB_NOMBRE', 'tragamillas2');
+//define('DB_PASSWORD', 'Admin1234');
 
 define('RUTA_Icon', RUTA_URL . '/public/img/icons/');
 define('RUTA_Foto', RUTA_URL . '/public/img/fotos/');
--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-# tragamillas
+# Containers
 ```bash
-docker run --name nginxSBR -d -p 80:80 -p 443:443 -v nginx-web:/var/www/html:ro -v nginx-conf:/etc/nginx/sites-enabled:ro richarvey/nginx-php-fpm
+docker run --name tragamillas -d -p 80:80 -p 443:443 -v nginx-web:/var/www/html:ro -v nginx-conf:/etc/nginx/sites-enabled:ro richarvey/nginx-php-fpm
 ```
 ```bash
-docker run --name mysqlSBR -p 3306:3306 -v mysql-bbdd:/var/lib -v mysql-conf:/etc/mysql -e MYSQL_ROOT_PASSWORD=alumno -d mysql
+docker run --name mysql -p 3306:3306 -v mysql-bbdd:/var/lib -v mysql-conf:/etc/mysql -e MYSQL_ROOT_PASSWORD=toor -d mysql
 ```
 ```bash
-docker run --name myadmSBR -d --link mysqlSBR:db -p 8080:80 phpmyadmin
+docker run --name pma -d --link mysqlSBR:db -p 8080:80 phpmyadmin
+```
+
+# Networks
+```bash
+docker network create tragamillas
+```
+```bash
+docker network connect tragamillas tragamillas
+```
+```bash
+docker network connect tragamillas mysql
+```
+```bash
+docker network connect tragamillas pma
 ```
